5469abce4cf75123784c2c9f7fa18196e24f3774
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2024-08-30 18:05:43 +0200

    Project structure created.

---
+++ sphinx/conf.py
@@ -0,0 +1,66 @@
+# Configuration file for the Sphinx documentation builder.
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+import tomli
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+# Function to read version from pyproject.toml
+def get_version_from_pyproject():
+    pyproject_path = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
+    with open(pyproject_path, "rb") as pyproject:
+        pyproject_data = tomli.load(pyproject)
+    return pyproject_data["tool"]["poetry"]["version"]
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "ontouml-models-lib"
+copyright = "Pedro Paulo F. Barcelos <p.p.favatobarcelos@utwente.nl>"
+author = "Pedro Paulo F. Barcelos <p.p.favatobarcelos@utwente.nl>"
+
+# Extract version and release from pyproject.toml
+version = get_version_from_pyproject()
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "autoapi.extension",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.githubpages",
+    "sphinx_rtd_size",
+    "sphinx_toolbox.sidebar_links",
+]
+
+autodoc_typehints = "description"
+github_username = "pedropaulofb"
+github_repository = "ontouml/ontouml-models-lib"
+
+autoapi_dirs = ["../ontouml_models_lib/"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_logo = "https://raw.githubusercontent.com/OntoUML/ontouml-models-lib/main/resources/ontouml-models-lib-logo.png"
+html_theme_options = {"display_version": True}
+
+sphinx_rtd_size_width = "100%"
+
+html_css_files = [
+    "logo_resize.css",
+]
---
+++ sphinx/index.rst
@@ -0,0 +1,33 @@
+Welcome to ontouml-models-lib's code documentation!
+=============================================
+
+.. raw:: html
+
+    <div class="custom-logo">
+        <img src="../resources/ontouml-models-lib-logo.png" alt="Library Logo">
+    </div>
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+ontouml-models-lib Websites
+===================
+
+.. raw:: html
+
+    <div style="text-align: left; margin-bottom: 20px;">
+        <a href="https://github.com/ontouml/ontouml-models-lib">
+            <img src="https://raw.githubusercontent.com/ontouml/ontouml-models-lib/main/resources/github-logo.png" width="20" align="middle"> View on GitHub </a>
+    </div>
+    <div style="text-align: left;">
+        <a href="https://pypi.org/project/ontouml-models-lib">
+            <img src="https://raw.githubusercontent.com/ontouml/ontouml-models-lib/main/resources/pypi-logo.png" width="20" align="middle"> View on PyPI </a>
+    </div>
